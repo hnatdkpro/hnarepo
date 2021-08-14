70f428ed260bc9e2485c1cae3a78c82ac867eba3
hnatdkpro <70943610+hnatdkpro@users.noreply.github.com>
2021-08-14 22:57:41 +0630

    Add files via upload

--- cyshome.php
+++ cyshome.php
@@ -1,296 +1,149 @@
-<!DOCTYPE html>
-<html>
-<head>
+<?php 
 
-
-	<title>Care Your Skin Myanmar</title>
-
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<!--For Responsive Design-->
-	<!-- Latest compiled and minified CSS -->
-
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"intergrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-	<!-- Optional theme -->
-
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-	<!-- Latest compiled and minified JavaScript -->
-
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-</head>
-<body>
-	<h1>CYS Online Skin Care Shop</h1>
-
-<!--CYS Navigation-->
-	<nav class="navbar navbar-light" style="background-color: #02ffc5;">
-	<div class="container-fluid">
-		<div class="navbar-header">
-			<a class="navbar-brand" href="index.html">CARE YOUR SKIN MYANMAR</a>
-	</div>
-		<ul class="nav navbar-nav">
-			<li class="active"><a href="cyshome.php">HOME</a></li>
-			<li><a href="#items">ITEMS</a></li>
-			<li><a href="#aboutsj">ABOUT SJ</a></li>
-			<li><a href="#contactus">CONTACT US</a></li>
-		</ul>
-	</div>
-</nav>
-<!--End of CYS Navigation-->
-
-<!--CYS Image Banner-->
-<div class="container-fluid">
-	<div class="row">
-		<img src="images/logo.png" class="img-responsive">
-	</div>
-</div>
-<!--End of CYS Image Banner-->	
-
-<div class="jumbotron">
-  <h1 class="display-4">Hello, Customer</h1>
-  <p class="lead">This is Our Skin Care Reseller Page</p>
-  <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-  </p>
-</div>
-
-<!--CYS ITEMS-->
-
-<div id="items">
-	<div class="container-fluid">
-		<h1 style="color: gold;">ITEMS</h1>
-	<div class="row">
+/**
+ * 
+ */
+class cyshome extends CI_Controller
+{
+	public function index()
+	{
+		$this->load->model("home_model");  
+        $data["fetch_member_data"] = $this->home_model->fetch_member_data();  
+        $this->load->view("cyshome", $data);  
+	}
 	
-		<div class="col-md-4 col-sm-12">
-			<h2>Hyaluric Acid</h2>
-			<p> 30,000 MMK </p>
-			<button class="btn btn-primary">Buy Now</button>
-			<br><br>
-			<img src="images/acid.jpg" class="img-responsive">
-		</div>
 
-		<div class="col-md-4 col-sm-12">
-			<h2>Day and Night Cream</h2>
-			<p> 30,000 MMK </p>
-			<button class="btn btn-primary">Buy Now</button>
-			<br><br>
-			<img src="images/cream.jpg" class="img-responsive">
-		</div>
-
-		<div class="col-md-4 col-sm-12">
-			<h2>Eye Cream</h2>
-			<p> 15,000 MMK </p>
-			<button class="btn btn-primary">Buy Now</button>
-			<br><br>
-			<img src="images/eye.jpg" class="img-responsive">
-		</div>
-
-		<div class="col-md-4 col-sm-12">
-			<h2>Facial Form and Toner</h2>
-			<p> 30,000 MMK </p>
-			<button class="btn btn-primary">Buy Now</button>
-			<br><br>
-			<img src="images/facial.jpg" class="img-responsive">
-		</div>
-
-		<div class="col-md-4 col-sm-12">
-			<h2>Hyaluric Lipstick</h2>
-			<p> 30,000 MMK </p>
-			<button class="btn btn-primary">Buy Now</button>
-			<br><br>
-			<img src="images/lipstick.jpg" class="img-responsive">
-		</div>
-
-		<div class="col-md-4 col-sm-12">
-			<h2>Bird Nest Mask</h2>
-			<p> 30,000 MMK </p>
-			<button class="btn btn-primary">Buy Now</button>
-			<br><br>
-			<img src="images/mask.jpg" class="img-responsive">
-		</div>
-	
-	</div>
-</div>
-</div>
-<!--End of CYS ITEMS-->
-
-<!--CYS ITEMS VER 2-->
-
-<div id="">
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-md-6 col-wm-12">
-				<img src="images/mask.jpg" class="img-responsive">
-			</div>
-			<div class="col-md-6 col-wm-12">
-				<h1>Snow Lotus Mask</h1>
-				
-				<button type="button" class="btn btn-success">Buy Now</button>
-				
-			</div>
-		</div>
-	</div>
-</div>
-
-<!--About Us-->
-
-<div id="aboutsj">
-	<div class="container-fluid">
-		<h1 style="color: gold;">About SJ</h1>
-	<div class="row">
-	
-		<div class="col-md-4 col-sm-12">
-			<h2>China Founder</h2>
-			<p> Fengtian </p>
-			<img src="images/chinafounder.jpg" class="img-responsive">
-		</div>
-
-		<div class="col-md-4 col-sm-12">
-			<h2>Myanmar Founder</h2>
-			<p> Hsu Hnin Khaing </p>
-			<img src="images/myanmarfounder.jpg" class="img-responsive">
-		</div>
-
-		<div class="col-md-4 col-sm-12">
-			<h2>CYS Owner</h2>
-			<p> Htet Naing Aung </p>
-			<img src="images/cysowner.png" class="img-responsive">
-		</div>
-
-	</div>
-</div>
-</div>
-
-<!--End of About Us-->
-
-<br>
-
-<!-- Contact Us & Login Form-->
-	<div id="contactus">
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-md-4 col-sm-12">
-					<h1 style="color: gold;">Contact Us</h1>
-					<address> Address: TaReinBaYan Road, Tarmwe Tsp, Yangon, Myanmar. </address>
-					<p>Phone Number: 09260256895</p>
-					<p>Email: careyourskinmyanmar@gmail.com</p>
-				</div>
-
-				<div class="col-md-4 col-sm-12">
-					<img src="images/shop.jpg" class="img-responsive">
-				</div>
+	public function    construct()
+		{
+			parent::   Construct();
+			$this->load->helper('url');
+		}
 
 
-				<div class="col-md-4 col-sm-12">
-										<form action="<?php echo base_url();?>cyshome/login_validation" method="post">
-						<legend>Login</legend>
-							<div class="form-group">
-								<label>Username:</label>
-								<input type="text" name="username" class="form-control" required="required" placeholder="Enter Username" autocomplete="off" autofocus="autofocus">
-								<span class="text-danger"><?php echo form_error('username');?> </span>
-							</div>
-								<div class="form-group">
-								<label>Password:</label>
-								<input type="Password" name="password" class="form-control" required="required" placeholder="Enter Password" autocomplete="off" autofocus="autofocus">
-								<span class="text-danger"><?php echo form_error('password');?> </span>
-								</div>
-								<div class="form-group">
-									<input type="submit" name="insert" value="Login" class="btn btn-info">
-								</div>
-					</form>
-					
-				</div>
+//this is login validation
 
+	public function login_validation()
+	{
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('username','Username','required|alpha');
+		$this->form_validation->set_rules('password','Password','required|alpha');
+		if($this->form_validation->run())
+	{
+		$username=$this->input->post('username');
+		$password=$this->input->post('password');
+		$this->load->model('home_model');
+		if($this->home_model->check_login($username,$password))
+	{
+		$session_data=array('username'=> $username);
+		$this->session->set_userdata($session_data); 
+		redirect(base_url().'cyshome/enterdashboard');
+	}
+		else
+	{
+		$this->session->set_flashdata('error','Invalid username and password');
+		redirect(base_url().'cyshome/login');
+	}
+	}
+		else
+	{
+		$this->index();
+	}
+	}
 
-		</div>
-	</div>
+//this is form validation
 
-	<br><br>
+	public function form_validation()
+	{
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules("name","Name","required|alpha");
+		$this->form_validation->set_rules("address","Address","required|alpha");
+		if($this->form_validation->run())
+	{
+		$this->load->model("home_model");
+	}
+		else
+	{
+		$this->index();
+	}
+	}
 
-//<!-- Location -->
+// This is enter dashboard
 
-	<div class="row">
-		<div class="col-md-6 col-sm-12">
-			<h1 style="color: gold;">Shop Location</h1>
-			<div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Tarmwe+(Care%20Your%20Skin%20Myanmar)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://www.mapsdirections.info/en/measure-map-radius/">Draw Map Circle</a></div>s
-		</div>
+		public function enterdashboard()
+	{
+		if($this->session->userdata('username')!='')
+	{
+		redirect(base_url().'cysapanel/index');
 		
-		<div class="col-md-6 col-sm-12">
-			<h1 style="color: gold;">Check Your Location</h1>
-				<button ondclick="getLocation()">Show My Location
-				</button>
-				<p id="demo">
-				</p>
-				<script>
-					var x = document.getElementById("demo");
-					function getLocation() {
-						if (navigator.geolocation) {
-							navigator.geolocation.watchPosition(showPosition);
-							p=x.innerHTML;a
-						} else {
-							x.innerHTML = "Geolocation is not supported by this browser.";
-								}
-						}
-					function showPosition(position) {
-						x.innerHTML="Latitude:" + position.coords.latitude + "Longitude" + position.coords.longitude;
-					}
-				
-				</script>
-				<div style="width: 100%" class="img-responsive">
-				<iframe width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="htttps://maps.google.com/maps?q=p&amp;output=embed"></iframe>
-				<a href= "https://www.mapsdirections.info/en/measure-map-radius/">
-				</a>
-				</div>
-		</div>
-	</div>
-</div>
-
-//<!-- CYS VIDEO -->
-
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-6 col-wm-12">
-			<h1 style="color: gold;">This is Unboxing Video of Our Product</h1>
-			<h2 style="color: darkgrey;">Shenjing Snow Lotus Mask</h2>
-			<p>Find Us on Youtube</p>
-		</div>
-		<div class="col-md-6 col-wm-12">
-			<div class="embed-responsive embed-responsive-4by3">
-				<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/K-wRyEm7Yag" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-			</div>
-		</div>
-	</div>
-</div>
-
-<br><br>
-
-//<!-- CYS Footer -->
-	
-<footer>
-	<p style="text-align: center;"><a href="cyshome.php">Home</a></p>
-	<p style="text-align: center;"><a href="#items">ITEMS</a></p>
-	<p style="text-align: center;"><a href="#aboutsj">About SJ</a></p>
-	<p style="text-align: center;"><a href="#contactus">CONTACT Us</a></p>
-	<p style="text-align: center;"><a href="#login">Login</a></p>
-	
-	<div style="text-align: center; font-size: 1.5em;">
-		<a href="https://www.facebook.com/careyourskinmyanmar" class="fa fa-facebook"></a>
-		<a href="https://www.google.com" class="fa fa-google"></a>
-		<a href="https://m.youtube.com/channel/UC08GbDCeAO581jwFPgG3d6A/videos" class="fa fa-youtube"></a>
-		<a href="https://mail.google.com" class="fa fa-envelope"></a>
-	</div>
-	<p style="text-align: center;">&copy; Copyright 2021 careyourskinmyanmar</p>
-</footer>
-
-//<!--End of Footer-->
+		echo '<h2>Welcome - '.$this->session->userdata('username').'</h2>'; 
+		echo '<label><a href="'.base_url().'cyshome/logout">Logout</a></label>';
+	}
+		else
+	{
+		redirect(base_url().'cyshome/login');
+	}
+	}
 
 
-</body>
-</html>
+// this is login and logout
+
+	public function login()
+	{
+		$this->load->view("cyshome");
+	}
+
+	function logout()
+	{
+		$this->session->unset_userdata('username');
+		redirect(base_url().'cyshome/login');
+	}
+
+	public function go_insert()  
+    {  
+        //echo 'OK'; 
+        $this->load->model("home_model");
+        $membername = $this->input->post("membername");
+        $address = $this->input->post("address");
+        $phone = $this->input->post("phone");
+
+ 
+
+    	echo $membername; 
+    	echo $address;
+    	echo $phone;
+
+    	$data = array('membername' => $this->input->post("membername"),'address' =>$this->input->post("address"),'phone'=>$this->input->post("phone") );
+
+    	echo $data;
+
+    	$this->home_model->insert_data($data);  
+                     redirect(base_url() . "cyshome/inserted");  
+    	
+    }  
+
+
+
+    public function inserted()  
+    {  
+        $this->index();  
+    }  
+
+    public function delete_data(){  
+
+    	$this->load->model("home_model");
+        $id = $this->input->post("id");
+        echo $id;
+           $id = $this->input->post("id"); 
+           $this->load->model("home_model");  
+           $this->home_model->delete_data($id);  
+           redirect(base_url() . "cyshome/deleted");  
+      } 
+
+       public function deleted()  
+      {  
+           $this->index();  
+      }
+
+
+ }
+ ?>
